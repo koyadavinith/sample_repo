3bab8b4471878ae476838c2df0c8144421bb0003
vinith <vinthkoyada@gmail.com>
2023-09-18 21:49:58 +0530

    Added a comment

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+# temp comment
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '0db8b73bb1f69844946b2360'
